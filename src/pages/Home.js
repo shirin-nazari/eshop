3f1ccd6be7f6c44075479b8b9bdad3172b7e165f
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-04-30 20:47:25 +0430

    add card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
+import ImageOne from '../assets/140Z_2106.w023.n001.614B.p1.614.jpg';
+import ImageTwo from '../assets/6508430.jpg';
+import ImageThree from '../assets/3255317.jpg';
 import 'react-awesome-slider/dist/styles.css';
+import Card from '../component/card/Card';
+const styles = {
+  marginTop: '5rem',
+};
 
 function Home() {
   return (
-    <>
+    <section style={styles}>
       <AwesomeSlider>
-        <div data-src="/path/to/image-0.png" />
-        <div data-src="/path/to/image-1.png" />
-        <div data-src="/path/to/image-2.jpg" />
+        <div data-src={ImageOne} />
+        <div data-src={ImageTwo} />
+        <div data-src={ImageThree} />
       </AwesomeSlider>
-    </>
+      <section>
+        <Card /> <Card />
+        <Card /> <Card />
+      </section>
+    </section>
   );
 }
 
