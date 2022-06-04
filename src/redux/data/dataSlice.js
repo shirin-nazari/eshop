af04bb9d22e5026da81b16e14f16ad45bcf46d51
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-06-04 22:50:44 +0430

    fix error

--- src/redux/data/dataSlice.js
+++ src/redux/data/dataSlice.js
@@ -1,13 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import dataFile from '../../data.json';
+// import dataFile from '../../data.json';
 export const fetchData = createAsyncThunk('data', async () => {
   // return await fetch('https://fakestoreapi.com/products/')
   //   .then((res) => res.json())
   //   .catch((err) => console.log('Error ::', err));
   // return await fetch(dataFile)
-  return await fetch(dataFile)
+  return await fetch('data.json')
     .then((res) => {
-      return res;
+      console.log(res);
+      return res.json();
     })
     .catch((err) => console.log(err));
 });
