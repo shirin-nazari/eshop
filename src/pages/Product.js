7d19dd81582739124f1f6490ecb3533855fe8d1d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-06-11 23:42:29 +0430

    (:

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -16,44 +16,41 @@ function Product() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.data);
   const searched = useSelector((state) => state.data);
-  console.log(data);
   useEffect(() => {
     dispatch(fetchData());
   }, []);
   return (
     <>
       <BoxCard>
-        {searched ===
-          null?.data.map(
-            ({ category, id, description, image, price, rating }) => {
-              console.log(category, id, price);
-              return (
-                <Card
-                  key={id}
-                  imageSrc={image}
-                  title={category}
-                  textDescription={description}
-                  price={price}
-                  rating={rating.rate}
-                />
-              );
-            }
-          )}
-        :
-        {data.data.map(
-          ({ category, id, description, image, price, rating }) => {
-            return (
-              <Card
-                key={id}
-                imageSrc={image}
-                title={category}
-                textDescription={description}
-                price={price}
-                rating={rating.rate}
-              />
-            );
-          }
-        )}{' '}
+        {searched.length > 1
+          ? searched.data.map(
+              ({ category, id, description, image, price, rating }) => {
+                return (
+                  <Card
+                    key={id}
+                    imageSrc={image}
+                    title={category}
+                    textDescription={description}
+                    price={price}
+                    rating={rating.rate}
+                  />
+                );
+              }
+            )
+          : data.data.map(
+              ({ category, id, description, image, price, rating }) => {
+                return (
+                  <Card
+                    key={id}
+                    imageSrc={image}
+                    title={category}
+                    textDescription={description}
+                    price={price}
+                    rating={rating.rate}
+                  />
+                );
+              }
+            )}
       </BoxCard>
     </>
   );
